26247fc150ffacf91147d433735cb1fa97e12d24
Kumar-Abhimanyu <kumar.abhimanyu.git@gmail.com>
2024-02-26 12:31:55 +0530

    Kotlin Examples

--- src/Lamdas/LamdasDemo.java
+++ src/Lamdas/LamdasDemo.java
@@ -1,5 +1,8 @@
 package Lamdas;
 
+import java.awt.print.Printable;
+import java.util.function.Predicate;
+
 public class LamdasDemo {
     public static void main(String[] args) {
         SayableImpl sayable = new SayableImpl();
@@ -9,5 +12,14 @@ public class LamdasDemo {
             System.out.println("Say something");
         };
         s1.say();
+
+//        Printable pr1 = (pmsg)-> {
+//            System.out.println("The message is "+pmsg);
+//        };
+//        pr1.printMsg()
+
+        //Predefined SAMs
+        Predicate<Integer> pr = (tInt)->(tInt>18);
+        System.out.println(pr.test(19));
     }
 }
